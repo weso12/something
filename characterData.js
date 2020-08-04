69e264b1cea9ee8208d787ba9e24c88a388c65c4
weso12 <wesleytoma@gmail.com>
2020-08-04 02:49:05 -0700

    Initial Commit

---
+++ characterData.js
@@ -0,0 +1,41 @@
+var characters = {
+	"Marth": {
+		weaponType: [
+			{
+				type: "Sword",
+				rank: "S"
+			},
+			{
+				type: "Rapier",
+				rank: "S"
+			}
+		],
+		armorType: [
+			{
+				type: "Light Armor",
+				rank: "S"
+			},
+			{
+				type: "Shield",
+				rank: "B"
+			}
+		],
+		skillsType: [
+			{
+				type: "Combat",
+				rank: "A"
+			},
+			{
+				type: "Fencing",
+				rank: "S"
+			},
+			{
+				type: "Charm",
+				rank: "S"
+			}
+		],
+		series: "Fire Emblem",
+		games: ["Fire Emblem: Shadow Dragon", "Fire Emblem: Mystery of the Emblem", "Super Smash Bros. Melee", "Super Smash Bros. Brawl", "Super Smash Bros. 4", "Super Smash Bros. Ultimate"],
+		tags: ["Male", "Human", "Humanoid", "Blue Hair"]
+	},
+}
---
+++ functions.js
@@ -0,0 +1,5 @@
+var gold = 0;
+
+function displayGold() {
+	document.getElementById("goldNumber").innerHTML = gold
+}
